b9f479c7412fa3d70d1d30223d5ab6c29f344c9b
Jaagrav <jaagravseal03@gmail.com>
2021-01-09 19:40:01 +0530

    Created the tasks app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import Home from "./screens/home";
+import Edit from "./screens/edit";
 
 function App() {
+  const [tasks, setTasks] = React.useState([
+    {
+      text: "Get a haircut",
+      isDone: false,
+    },
+    {
+      text: "Buy some milk",
+      isDone: false,
+    },
+  ]);
+
+  React.useEffect(() => {
+    if (localStorage.getItem("tasks"))
+      setTasks(JSON.parse(localStorage.getItem("tasks")));
+  }, []);
+
+  React.useEffect(() => {
+    if (localStorage) localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => <Home tasks={tasks} setTasks={setTasks} />}
+          />
+          <Route
+            exact
+            path="/edit"
+            component={() => <Edit tasks={tasks} setTasks={setTasks} />}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
